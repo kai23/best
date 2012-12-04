91e108492313679e4d32736e587246a543083eff
Florian <Florian.chevallier@ingesup.com>
2012-12-04 19:53:11 +0100

    adieux !

---
+++ BestGreenIT/src/com/ingesup/greenIT/Todo.java
@@ -0,0 +1,48 @@
+package com.ingesup.greenIT;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Todo implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String summary;
+	private String description;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	@Override
+	public String toString() {
+		return "Todo [id=" + id + ", summary=" + summary + ", description="
+				+ description + "]";
+	}
+	
+	public Todo(){super();};
+
+}
